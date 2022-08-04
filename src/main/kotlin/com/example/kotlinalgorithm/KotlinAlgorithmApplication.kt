3ca9cef0c86a1eed293275eb6f223d34db5d6b3d
ljw0829 <ljw0829@midasin.com>
2022-08-04 19:35:04 +0900

    - feat : 알고리즘 프로젝트 기본 세팅

---
+++ src/main/kotlin/com/example/kotlinalgorithm/KotlinAlgorithmApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinalgorithm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAlgorithmApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinAlgorithmApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinalgorithm/KotlinAlgorithmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinalgorithm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAlgorithmApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
