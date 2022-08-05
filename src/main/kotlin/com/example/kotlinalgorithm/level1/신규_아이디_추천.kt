8dfa0045c3ecbef9407d762474c6aaf7c42ca034
ljw0829 <ljw0829@midasin.com>
2022-08-05 20:07:49 +0900

    - feat : 신규 아이디 추천

---
+++ src/main/kotlin/com/example/kotlinalgorithm/level1/신규_아이디_추천.kt
@@ -0,0 +1,29 @@
+package com.example.kotlinalgorithm.level1
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/72410
+ */
+class 신규_아이디_추천 {
+
+    fun solution(new_id: String): String {
+        return new_id.lowercase()
+                .filter { it in 'a'..'z' || it in '0'..'9' || it in "-_." }
+                .replace(Regex("""\.+"""), ".")
+                .removePrefix(".")
+                .removeSuffix(".")
+                .run { ifEmpty { "a" } }
+                .take(15)
+                .removeSuffix(".")
+                .let {
+                    when (it.length) {
+                        1 -> it.plus(it.last()).plus(it.last())
+                        2 -> it.plus(it.last())
+                        else -> it
+                    }
+                }
+    }
+
+}
+
+
+
