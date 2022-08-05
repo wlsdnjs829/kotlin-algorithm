af8e19cd4e793cde27db5aa535319577fb3ba61b
ljw0829 <ljw0829@midasin.com>
2022-08-05 20:14:03 +0900

    - refactor : 신규 아이디 추천

--- src/main/kotlin/com/example/kotlinalgorithm/level1/신규_아이디_추천.kt
+++ src/main/kotlin/com/example/kotlinalgorithm/level1/신규_아이디_추천.kt
@@ -5,23 +5,21 @@ package com.example.kotlinalgorithm.level1
  */
 class 신규_아이디_추천 {
 
-    fun solution(new_id: String): String {
-        return new_id.lowercase()
-                .filter { it in 'a'..'z' || it in '0'..'9' || it in "-_." }
-                .replace(Regex("""\.+"""), ".")
-                .removePrefix(".")
-                .removeSuffix(".")
-                .run { ifEmpty { "a" } }
-                .take(15)
-                .removeSuffix(".")
-                .let {
-                    when (it.length) {
-                        1 -> it.plus(it.last()).plus(it.last())
-                        2 -> it.plus(it.last())
-                        else -> it
-                    }
+    fun solution(new_id: String) = new_id.lowercase()
+            .filter { it in 'a'..'z' || it in '0'..'9' || it in "-_." }
+            .replace(Regex("""\.+"""), ".")
+            .removePrefix(".")
+            .removeSuffix(".")
+            .run { ifEmpty { "a" } }
+            .take(15)
+            .removeSuffix(".")
+            .let {
+                when (it.length) {
+                    1 -> it.plus(it.last()).plus(it.last())
+                    2 -> it.plus(it.last())
+                    else -> it
                 }
-    }
+            }
 
 }
 
