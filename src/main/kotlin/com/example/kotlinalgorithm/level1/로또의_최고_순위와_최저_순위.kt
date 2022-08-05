8f39548a0b493ec5aca941ed43f83cb50053e0bc
ljw0829 <ljw0829@midasin.com>
2022-08-05 19:21:22 +0900

    - refactor : 로또의 최고 순위와 최저 순위 (다른 소스 참조)

--- src/main/kotlin/com/example/kotlinalgorithm/level1/로또의_최고_순위와_최저_순위.kt
+++ src/main/kotlin/com/example/kotlinalgorithm/level1/로또의_최고_순위와_최저_순위.kt
@@ -5,25 +5,32 @@ package com.example.kotlinalgorithm.level1
  */
 class 로또의_최고_순위와_최저_순위 {
 
-    fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
-        val joker = lottos.count { it == 0 }
-
-        val containsCount = lottos.count { win_nums.contains(it) }
-
-        val topRank = getRank(containsCount + joker)
-        val lowestRank = getRank(containsCount)
-
-        return intArrayOf(topRank, lowestRank)
+    fun solution(lottos: IntArray, winNums: IntArray): IntArray {
+        return intArrayOf(
+                (lottos.size.plus(1)) - lottos.filter { winNums.contains(it) || it == 0 }.size,
+                (lottos.size.plus(1)) - lottos.filter(winNums::contains).size
+        ).map { if (it > 6) it - 1 else it }.toIntArray()
     }
 
-    private fun getRank(containsCount: Int) = when (containsCount) {
-        6 -> 1
-        5 -> 2
-        4 -> 3
-        3 -> 4
-        2 -> 5
-        else -> 6
-    }
+//    fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
+//        val joker = lottos.count { it == 0 }
+//
+//        val containsCount = lottos.count { win_nums.contains(it) }
+//
+//        val topRank = getRank(containsCount + joker)
+//        val lowestRank = getRank(containsCount)
+//
+//        return intArrayOf(topRank, lowestRank)
+//    }
+//
+//    private fun getRank(containsCount: Int) = when (containsCount) {
+//        6 -> 1
+//        5 -> 2
+//        4 -> 3
+//        3 -> 4
+//        2 -> 5
+//        else -> 6
+//    }
 
 }
 
