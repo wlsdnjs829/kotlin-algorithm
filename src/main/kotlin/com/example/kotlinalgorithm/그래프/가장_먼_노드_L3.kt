a60cd4a8ec338e6b2a0e3698ebd067e1ca8bd03c
이 진원 <olo_jinwon@naver.com>
2022-09-22 20:39:01 +0900

    - feat : 가장 먼 노드

---
+++ src/main/kotlin/com/example/kotlinalgorithm/그래프/가장_먼_노드_L3.kt
@@ -0,0 +1,40 @@
+package com.example.kotlinalgorithm.그래프
+
+fun main() {
+    println(가장_먼_노드_L3().solution(6, arrayOf(intArrayOf(3,6), intArrayOf(3, 2), intArrayOf(1, 3), intArrayOf(1, 2), intArrayOf(5, 2), intArrayOf(6, 4))))
+}
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=kotlin
+ */
+class 가장_먼_노드_L3 {
+
+    private val removeNode = mutableSetOf<Int>()
+
+    fun solution(n: Int, edge: Array<IntArray>) =
+            edge.flatMap { setOf(it[0] to it[1], it[1] to it[0]) }
+                    .groupBy { it.first }
+                    .map { entry -> entry.key to entry.value.map { it.second }.toSet() }
+                    .toMap()
+                    .run {
+                        val values = setOf(1)
+                        removeNode.add(1)
+                        depth(this, values)
+                    }
+
+    private tailrec fun depth(map: Map<Int, Set<Int>>, values: Set<Int>): Int {
+        val otherDepth = map.filterKeys { values.contains(it) }
+                .values
+                .flatMap { it.toMutableList() }
+                .filter { !values.contains(it) && !removeNode.contains(it) }
+                .toSet()
+
+        return if (otherDepth.isEmpty()) {
+            values.size
+        } else {
+            removeNode.addAll(otherDepth)
+            depth(map, otherDepth.toSet())
+        }
+    }
+
+}
